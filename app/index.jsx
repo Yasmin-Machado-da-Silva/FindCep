65e8e043cd585c79bbeb0f40ef0e24424730c918
Yasmin Machado <yasmin.m.silva14@aluno.senai.br>
2025-08-12 16:20:20 -0300

    Feat✨: Ajustando botão e banner

--- app/index.jsx
+++ app/index.jsx
@@ -1,5 +1,6 @@
 import { Text, View, StyleSheet, ImageBackground, Image } from "react-native";
 import { Input } from '../components/input/input';
+import { Botao } from '../components/botao/botao';
 
 export default function Index() {
   return (
@@ -18,14 +19,14 @@ export default function Index() {
         {/* 2.1. Titulo */}
         <Text style={styles.titulo}>Consulte seu CEP</Text>
         {/*  */}
-        
+
+        {/* 2.2 Input */}
         <Input />
+        {/* 2.3 Botão */}
+        <Botao tituloBotao='Consultar' />
+        {/* 2.4 Card de informação */}
 
       </View>
-      {/* 2.1 Título */}
-      {/* 2.2 Input */}
-      {/* 2.3 Botão */}
-      {/* 2.4 Card de informação */}
     </>
   );
 }
